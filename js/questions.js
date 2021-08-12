e8297b2afc14b0c156f4b3a7f46d856dc61ef923
kedar051 <kedarjadhav051@gmail.com>
2021-08-12 21:44:50 +0530

    Added

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a Java features?",
+    answer: "Use of pointers",
+    options: [
+      "Dynamic",
+      "Architecture Neutral",
+      "Use of pointers",
+      "Object-oriented"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "_____ is used to find and fix bugs in the Java programs.",
+    answer: "JDB",
+    options: [
+      "JDB",
+      "JVM",
+      "JRE",
+      "JDK"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In which memory a String is stored, when we create a string using new operator?",
+    answer: "Heap memory",
+    options: [
+      "Stack",
+      "String memory",
+      "Heap memory",
+      "Random storage space"
+    ]
+  },
+
+];
